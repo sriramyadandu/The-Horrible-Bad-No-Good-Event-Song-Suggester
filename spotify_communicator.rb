3d60f0f943449d280d31df1b08c8a9ff2fd16cc3
sriramya <sriramyadandu>
2020-08-12 12:33:49 -0400

    fixed params

--- spotify_communicator.rb
+++ spotify_communicator.rb
@@ -5,7 +5,6 @@ require 'json'
 Dotenv.load
 
 module SpotifyCommunicator
-    @SPOTIFY_BASE_URL = "https://api.spotify.com/v1/"
     @ENCODED_SECRETS = Base64.strict_encode64(ENV["SPOTIFY_CLIENT_ID"] + ":" + ENV["SPOTIFY_CLIENT_SECRET"])
     @AUTH = "Basic " + @ENCODED_SECRETS
     @AUTH_RESPONSE = HTTParty.post('https://accounts.spotify.com/api/token', body: {grant_type: 'client_credentials'}, headers: {Authorization: @AUTH})
@@ -15,7 +14,37 @@ module SpotifyCommunicator
         'Authorization' => ''+ @TOKEN_TYPE + " " + @ACCESS_TOKEN
     }
 
-    def query_encoder(wanted_words, unwanted_words = nil, additional_word = nil)
+    def search(q, type = [], limit = 20, offset = 0, include_external = nil, market = nil)
+        return if type.length == 0
+        
+        # Required params
+        search_query = {
+            :q => q,
+            :type => type.join(",")
+        }
+        
+        # Optional params
+        search_query[:limit] = limit
+        search_query[:offset] = offset
+        search_query[:include_external] = include_external unless include_external.nil?
+        search_query[:market] = market unless market.nil?
+        response = _get(_search_path, search_query)
+        puts response
+    end
+    
+    def recommendations(seed_artists, seed_genres, seed_tracks)
+        return if (seed_artists.split(',').length + seed_tracks.split(',').length + seed_genres.split(',').length) > 5
+        recommendations_query = {}
+        recommendations_query[:seed_artists] = seed_artists unless seed_artists.empty?
+        recommendations_query[:seed_tracks] = seed_tracks unless seed_tracks.empty?
+        recommendations_query[:seed_genres] = seed_genres unless seed_genres.empty?
+        response = _get(_recommendations_path, recommendations_query)
+        puts response
+    end
+    
+    private
+    
+    def self._query_encoder(wanted_words, unwanted_words = nil, additional_word = nil)
         full_query = ""
         full_query += wanted_words.join("+")
         full_query += "+NOT+" + unwanted_words.join("+NOT+") unless unwanted_words.nil?
@@ -24,50 +53,23 @@ module SpotifyCommunicator
         full_query
     end
 
-    def _search(q, type = [])
-        search_query = {
-            :q => q,
-            :type => type.join(",")
-            # :market =>
-            # :limit =>
-            # :offset =>
-            # :include_external =>
-        }
-        response = _get_search(_search_path, search_query)
-        puts response
+    def self._search_path
+        ENV["SPOTIFY_BASE_URL"] + "search"
     end
 
-    def _recommendations(seed_artists, seed_genres, seed_tracks)
-        return if (seed_artists.split(',').length + seed_tracks.split(',').length + seed_genres.split(',').length) > 5
-        recommendations_query = {}
-        recommendations_query[:seed_artists] = seed_artists unless seed_artists.empty?
-        recommendations_query[:seed_tracks] = seed_tracks unless seed_tracks.empty?
-        recommendations_query[:seed_genres] = seed_genres unless seed_genres.empty?
-        response = _get_recommendations(_recommendations_path, recommendations_query)
-        puts response
-    end
-    
-    def _search_path
-        @SPOTIFY_BASE_URL + "search"
+    def self._recommendations_path
+        ENV["SPOTIFY_BASE_URL"] + "recommendations"
     end
 
-    def _recommendations_path
-        @SPOTIFY_BASE_URL + "recommendations"
-    end
-
-    def _get_search(path, params)
-        HTTParty.get(path, :query => params, :headers => @HEADER)
-    end
-
-    def _get_recommendations(path, params)
+    def self._get(path, params)
         HTTParty.get(path, :query => params, :headers => @HEADER)
     end
     
-    def _post(path, payload)
+    def self._post(path, payload)
         ###
     end
     
-    def _put(path:, payload:)
+    def self_put(path:, payload:)
         ###
     end
 end
--- test_attempt.rb
+++ test_attempt.rb
@@ -1,5 +1,5 @@
 require_relative "spotify_communicator"
 include SpotifyCommunicator
 
-# SpotifyCommunicator::_search("pablo", ["artist","track","album","playlist"])
-# SpotifyCommunicator::_recommendations("4NHQUGzhtTLFvgF5SZesLK","classical,country","0c6xIDDpzE81m2q797ordA" )
+# SpotifyCommunicator::search("pablo", ["artist","track","album","playlist"])
+SpotifyCommunicator::recommendations("4NHQUGzhtTLFvgF5SZesLK","classical,country","0c6xIDDpzE81m2q797ordA" )
